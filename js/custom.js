c8647cff7f20ea28b5a2ff12b0864855360956ff
GiarcEnoredlac <calderonecraig@gmail.com>
2015-08-23 23:05:04 -0400

    photo update and split services content

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@
 (function($){
 	$(document).ready(function(){
 	
-		$(".banner-image").backstretch('images/banner.jpg');
+		$(".banner-image").backstretch('images/banner-single-grad.jpg');
 		
 		// Fixed header
 		// -----------------------------------------------
